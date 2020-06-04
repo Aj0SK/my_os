477b94ac8669a6861fb251138a8106de6c95023e
Aj0SK <andrejkorman@gmail.com>
2020-06-04 13:05:35 +0200

    Enter real mode

--- src/simple_boot_sector.asm
+++ src/simple_boot_sector.asm
@@ -1,46 +1,30 @@
-; Read some sectors from the boot disk using our disk_read function
+; A boot sector that enters 32 - bit protected mode.
 [org 0x7c00]
+mov bp, 0x9000 ; Set the stack.
+mov sp, bp
+mov bx, MSG_REAL_MODE
 
-;mov ah , 0x0e
-;mov bx, HELLO_MSG  
-;call print_string
-;mov bx, GOODBYE_MSG
-;call print_string
-
-mov [BOOT_DRIVE], dl ; BIOS stores our boot drive in DL , so it ’s
-; best to remember this for later.
-
-mov bp, 0x8000 ; Here we set our stack safely out of the
-mov sp, bp ; way , at 0 x8000
-
-mov bx, 0x9000 ; Load 5 sectors to 0 x0000 (ES ):0 x9000 (BX)
-mov dh, 5 ; from the boot disk.
-mov dl, [BOOT_DRIVE]
-call disk_load
-
-mov dx, [0x9000] ; Print out the first loaded word , which
-call print_hex ; we expect to be 0xdada , stored
-; at address 0 x9000
-
-mov dx, [0x9000 + 512] ; Also , print the first word from the
-call print_hex ; 2nd loaded sector : should be 0xface
+call print_string
+call switch_to_pm ; Note that we never return from here.
 
 jmp $
 
 %include "src/print_string.asm"
-%include "src/print_hex.asm"
-%include "src/disk_load.asm"
+%include "src/protected_mode/gdt.asm"
+%include "src/protected_mode/print_string.asm"
+%include "src/protected_mode/switch.asm"
 
-; Include our new disk_load function
+[bits 32]
+
+; This is where we arrive after switching to and initialising protected mode.
+BEGIN_PM:
+    mov ebx, MSG_PROT_MODE
+    call print_string_pm ; Use our 32 - bit print routine.
+    jmp $ ; Hang.
+
 ; Global variables
-BOOT_DRIVE:
-    db 0
+MSG_REAL_MODE db "Started in 16 - bit Real Mode" , 0
+MSG_PROT_MODE db "Successfully landed in 32 - bit Protected Mode" , 0
 ; Bootsector padding
-times 510 -( $ - $$ ) db 0
+times 510-($-$$) db 0
 dw 0xaa55
-; We know that BIOS will load only the first 512 - byte sector from the disk ,
-; so if we purposely add a few more sectors to our code by repeating some
-; familiar numbers , we can prove to ourselfs that we actually loaded those
-; additional two sectors from the disk we booted from.
-times 256 dw 0xdada
-times 256 dw 0xface
