f0785501fe427d37e52902f33807d2b9b4a58b65
Aj0SK <andrejkorman@gmail.com>
2020-06-07 21:05:28 +0200

    Fix print-string

--- src/boot/print_string.asm
+++ src/boot/print_string.asm
@@ -3,14 +3,15 @@ print_string:       ; expects string adress in bx
     push ax
 
     loop_through_string:
-    mov al, [bx]
-    mov ah, 0x0e    ; sets the right flag for interupt
-    int 0x10
-    add bx, 1       ; increment counter
-    mov al, [bx]    ; find if the current character
-    cmp al, 0       ; is not '\0'
-    jne loop_through_string ; jump if it is not the case
+        mov al, [bx]    ; find if the current character
+        cmp al, 0       ; is not '\0'
+        je end_loop_through_string ; jump if it is not the case
+        mov ah, 0x0e    ; sets the right flag for interupt
+        int 0x10
+        add bx, 1       ; increment counter
+        jmp loop_through_string
 
-    pop ax
-    pop bx
-    ret
+    end_loop_through_string:
+        pop ax
+        pop bx
+        ret
