c7f312665379dcb24ecdfe378e1a485ee58ddf91
Aj0SK <andrejkorman@gmail.com>
2020-07-04 09:05:23 +0200

    Update utils - add types and memset

--- src/kernel/util.c
+++ src/kernel/util.c
@@ -1,9 +1,17 @@
+#include "util.h"
+
 void memory_copy(char *source, char *dest, int nbytes)
 {
     for (int i = 0; i < nbytes; ++i)
     {
         *(dest + i) = *(source + i);
     }
+}
+
+void memory_set(u8 *dest, u8 val, u32 len)
+{
+    u8 *temp = (u8 *)dest;
+    for ( ; len != 0; len--) *temp++ = val;
 }
 
 void int_to_ascii(int n, char str[])
